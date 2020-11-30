446d41268b572b4cde6fa3f2fc9d873e2f683d80
sandar89 <pooh.sandar@gmail.com>
2020-11-30 22:04:23 +0630

    repair

--- app.js
+++ app.js
@@ -643,7 +643,7 @@ function webviewTest(sender_psid){
 start doctor 
 ****************/
 const appointment =(sender_psid) => {
-  let response1 = {"text": "Welcome to Thawdar Win Traditional Clinic"};
+  let response1 = {"text": "Welcome to Thawdar Win Clinic"};
   let response2 = {
     "text": "Please Select Doctor or Medicine",
     "quick_replies":[
